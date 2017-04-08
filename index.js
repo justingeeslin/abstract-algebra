f79b3555e6c276089775509b7b7b9aee35c61a0c
Justin Geeslin <justin@geesl.in>
2017-04-07 22:14:26 -0500

    Typo. Caps difference.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Create a list of Conversation Segment pairs. [ {key, url, title, timeStart, timeEnd, [{interest, score}]}]
-const Segment = require('./js/segment.js')
+const Segment = require('./js/Segment.js')
 var myFirstSegment = new Segment();
