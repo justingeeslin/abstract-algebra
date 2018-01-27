42fee0f7844f23ebde7712e600a72337913a9946
Justin Geeslin <justin@geesl.in>
2018-01-26 23:04:55 -0600

    Tests on Z4; Group under addition?

--- tests/index.js
+++ tests/index.js
@@ -56,6 +56,38 @@ describe('The set of the zero element', function() {
     expect(theArray.isField(operation, operation2)).toBe(true)
   })
 
+})
+
+describe('Z4', function() {
+
+  beforeAll(function() {
+    theArray = [0,1,2,3];
+
+    operation = function(element1, element2) {
+      return (element1 + element2) % theArray.length
+    }
+
+  })
+
+  it('should be closed', function() {
+    expect(theArray.isClosed(operation)).toBe(true)
+  })
+
+  it('should have an identity element', function() {
+    expect(theArray.hasIdentity(operation)).toBe(true)
+  })
+
+  it('should be associative', function() {
+    expect(theArray.isAssociative(operation)).toBe(true)
+  })
+
+  it('should have inverse', function() {
+    expect(theArray.hasInverse(operation)).toBe(true)
+  })
+
+  it('should be a group', function() {
+    expect(theArray.isGroup(operation)).toBe(true)
+  })
 
 })
 
